390254a8ba7dbd27a8e8178f58ef9656d7d021c7
Nick Lavery <tuneboynick@gmail.com>
2020-10-01 16:41:44 -0400

    Initial components

---
+++ src/components/SiteHero.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BackgroundImage = styled.section`
+    background-image:url("/images/image_1.jpg");
+`
+
+
+const SiteHero = () => {
+    return (
+        <div>
+            <BackgroundImage class="site-hero" id="section-home"
+                data-stellar-background-ratio="0.5">
+                <div class="container">
+                    <div class="row intro-text align-items-center justify-content-center">
+                        <div class="col-md-10 text-center pt-5">
+
+                            <h1 class="site-heading site-animate">Nickolaus<strong class="d-block">Lavery</strong>
+                            </h1>
+                            <strong class="d-block text-white text-uppercase letter-spacing">Welcome to my digital vitae page!</strong>
+
+                        </div>
+                    </div>
+                </div>
+            </BackgroundImage>
+        </div>
+    )
+}
+
+export default SiteHero;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+
+
 import * as serviceWorker from './serviceWorker';
+
+/*
+class MainApp extends React.Component {
+  render() {
+    return (
+      <div className="content">
+        <h1>TEST</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<MainApp />, document.getElementById("app"));
+*/
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('App')
 );
 
 // If you want your app to work offline and load faster, you can change
